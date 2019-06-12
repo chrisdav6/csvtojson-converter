29d359ec9eed7c20c3b905fe07aaf0f030e0b6d9
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-06-12 09:35:40 -0400

    Initial Commit

---
+++ converter.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const path = require("path");
+const convertCSV = require("csvtojson");
+
+
+//------NPM Module Code-------//
+const moduleCode = () => {
+  convertCSV()
+    .fromFile(path.join(__dirname, "customer-data.xls"))
+    .then((jsonObj) => {
+      fs.writeFile("module-converted.json", JSON.stringify(jsonObj), (err) => console.log(err));
+    });
+}
+
+moduleCode();
+
+
+//------Manual Code-------//
+//------Notice - This will not work if the csv file contains commas within the values----//
+const manualCode = () => {
+
+  fs.readFile(path.join(__dirname, "customer-data.xls"), "utf-8", (err, data) => {
+    if (err) return console.log(err);
+
+    //Split all lines into an array
+    var lines = data.split("\n");
+
+    //Remove all whitespace
+    lines = lines.map(function (line) {
+      return line.trim();
+    });
+
+    //Create an array to store objects
+    var result = [];
+
+    //Split headers into an array
+    var headers = lines[0].split(",");
+
+    //Loop over lines excluding header line - start at index 1
+    for (var i = 1; i < lines.length; i++) {
+
+      //Create object for each json index
+      var obj = {};
+
+      //Split each line into its own array
+      var currentline = lines[i].split(",");
+
+      //Loop over headers
+      for (var j = 0; j < headers.length; j++) {
+        //Assign headers as object properties and lines as values
+        obj[headers[j]] = currentline[j];
+      }
+
+      //Push json object into result array
+      result.push(obj);
+
+    }
+
+    //Stringify result
+    let final = JSON.stringify(result);
+
+    //Write converted file to disk
+    fs.writeFile("manual-converted.json", final, (err) => console.log(err));
+  });
+
+}
+
+manualCode();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Module 1 Assignment Lab: CSV to JSON Converter
+
+The assignment lab for this module is to build a node script to convert CSV files to JSON files.
+
+## My Solution
+
+I included two solutions. One using the NPM module [csvtojson](https://www.npmjs.com/package/csvtojson) and the other using pure JavaScript.
